3cb298ff9c48f1b2247dee0b62f8c4e7ad88787c
Andrew-John Hickman <60426517+ajth-work@users.noreply.github.com>
2020-04-27 16:10:46 +0900

    Add files via upload

--- practice-docs/sumAll.js
+++ practice-docs/sumAll.js
@@ -1,15 +1,20 @@
 function sumAll(num1, num2) {
   let sum = 0;
-  if ((num1 || num2) < 0) { 
+  if (((typeof num1) != "number") || (typeof num2 != "number")) {
     return "ERROR";
+  } else if ((num1 < 0) || (num2 < 0)) {
+    return "ERROR";
+  } else if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+      sum += i;
+    }
+  } else if (num2 < num1) {
+    for (let i = num2; i <= num1; i++) {
+      sum += i;
+    }
   }
-  for (let i = num1; i <= num2; i++) {
-    sum += i;
-  }
-  console.log("--------------")
   return sum;
 }
-
 
 console.log(sumAll(1, 4)) // .toEqual(10)
 console.log(sumAll(1, 4000)) // .toEqual(8002000);
