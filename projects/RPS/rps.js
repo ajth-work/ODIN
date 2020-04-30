08fa7af943b630c269c1b14e986388d88e5fa773
Andrew-John Hickman <60426517+ajth-work@users.noreply.github.com>
2020-04-30 16:20:34 +0900

    Add files via upload

--- projects/RPS/rps.js
+++ projects/RPS/rps.js
@@ -3,45 +3,40 @@ function computerPlay() {
     let choices, computerChoice;
     choices = ["rock", "paper", "scissors"]
     computerChoice = choices[Math.floor(Math.random() * choices.length)]
-    console.log(`Computer has made its move.`);
-    return computerChoice;
+	console.log(`Computer chose [${computerChoice}].`);
+	return computerChoice;
 };
-
-function playerPlay() {
+let btnResponse;
+function playerPlay(btnResponse) {
     // the player types their move, and if it is a simplified response, then the function renames it to the full move name for clarity
-    let playerChoice = prompt("Type Rock (r), Paper (p), or Scissors (s), please!").toLowerCase();
-    let playerChoiceRename;
-    if (playerChoice[0] == "r") {
-        playerChoiceRename = "rock";
-    } else if (playerChoice[0] == "p") {
-        playerChoiceRename = "paper";
-    } else if (playerChoice[0] == "s") {
-        playerChoiceRename = "scissors";
-    }
-    console.log(`Player chose [${playerChoiceRename}].`)
-    return playerChoiceRename;
+    let playerChoice = btnResponse;
+    console.log(`Player chose [${playerChoice}].`);
+    return playerChoice;
 };
 
 function playRPS(computerSelection, playerSelection) {
     // receives the inputs from the computer and the player, and evaluates whether the player won, lost, or tied against the computer
-    computerSelection = computerPlay();
-    playerSelection = playerPlay();
     let roundResult;
-    if (
-        (playerSelection[0] == "r" && computerSelection[0] == "s")
-        || (playerSelection[0] == "p" && computerSelection[0] == "r")
-        || (playerSelection[0] == "s" && computerSelection[0] == "p")
+    if ((computerPlay()!= undefined) && (playerPlay(btnResponse) != undefined)) {
+	if (
+        (playerSelection == "rock" && computerSelection == "scissors")
+        || (playerSelection == "paper" && computerSelection == "rock")
+        || (playerSelection == "scissors" && computerSelection == "paper")
     ) {
         roundResult = "win";
         console.log(`You won! You chose [${playerSelection}] and Computer chose [${computerSelection}].`)
-    } else if (playerSelection[0] == computerSelection[0]) {
+    } else if (playerSelection == computerSelection) {
         roundResult = "tie";
         console.log(`You tied. You and Computer both chose [${computerSelection}].`)
     } else {
         roundResult = "lose";
         console.log(`You lost! You chose [${playerSelection}] and Computer chose [${computerSelection}].`);
     } return roundResult;
+} else {
+	if (computerPlay()!= undefined) {alert("press start")}
+	else alert("choose between rock, paper, or scissors");
 }
+};
 
 function game() {
     // the function asks how many times to play, plays num amount of rounds, displays the entire match's current status, and exits
@@ -60,16 +55,34 @@ function game() {
     } return alert(`${num} round(s) completed! You won ${Math.floor(((winCount / num) * 100))}% of the round(s). Thanks for playing.`);
 }; 
 
-console.log(game());
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
+	const startButton = document.getElementById("start-button");
+	startButton.addEventListener('click', () => {
+		computerPlay();
+	});
+	
+	const rockButton = document.getElementById("rock-button");
+	rockButton.addEventListener('click', () => {
+	alert("You chose rock.")
+	btnResponse = "rock";
+	console.log(btnResponse)
+	return btnResponse;
+});
+	const paperButton = document.getElementById("paper-button");
+	paperButton.addEventListener('click', () => {
+	alert("You chose paper.")
+	btnResponse = "paper";
+	console.log(btnResponse)
+	return btnResponse;
+});
+	const scissorsButton = document.getElementById("scissors-button");
+	scissorsButton.addEventListener('click', () => {
+	alert("You chose scissors.")
+	btnResponse = "scissors";
+	console.log(btnResponse)
+	return btnResponse;
+});
+
+const submitButton = document.getElementById("round-count-submit-button");
+	submitButton.addEventListener('click', () => {
+		playRPS();
+	});
