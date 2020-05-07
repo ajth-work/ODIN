a180006664db77b1feb14f2c6238ffaae36f8996
ajth-work <ajth.work@gmail.com>
2020-05-08 08:37:20 +0900

    Started grid creation procedure

--- projects/etch-a-sketch/etch.js
+++ projects/etch-a-sketch/etch.js
@@ -1,29 +1,51 @@
 const containerSize = 500;
-const unitsWide = prompt("How many units wide?");
-const unitsTall = prompt("How many units tall?");
-const totalUnits = unitsWide * unitsTall;
-const pixelWidth = containerSize / unitsWide;
-const pixelHeight = containerSize / unitsTall;
+const units = prompt("How many units tall/wide?");
+//const units = 5; //test value
+const totalUnits = units * units;
+const pixel = containerSize / totalUnits;
+let pixelAreas = [];
 
-alert(`Creating a grid made of ${Math.floor(totalUnits)} units that are each ${Math.floor(pixelWidth)} pixels wide by ${Math.floor(pixelHeight)} pixels tall.`);
+console.log(`Creating a grid made of ${Math.floor(totalUnits)} units that are each ${Math.floor(pixel)} pixels wide and tall.`);
 
-// Making grid template text for columns
-let widthStr = pixelWidth + "px ";
-console.log(widthStr.length);
+// Making grid template text for columns and rows
+let pixelStr = pixel + "px ";
+console.log(pixelStr.length);
+pixelStr = pixelStr.padEnd((pixelStr.length) * units, pixelStr);
+console.log(pixelStr);
 
-widthStr = widthStr.padEnd((widthStr.length) * unitsWide, widthStr)
-alert(widthStr);
+// Making grid template areas 
+function prepAreas(num) {
+    for (let i = 1; i <= num; i++) {
+            pixelAreas = pixelAreas.concat(`div${i}`);
+        }
+    return pixelAreas
+};
+const prepAreasArray = prepAreas(totalUnits);
 
-// Making grid template text for rows
-let heightStr = pixelHeight + "px ";
-console.log(heightStr.length);
+function chunky(arr, size) {
+    let arrCopy = [...arr];
+    let newArray = [];
+    for (let i = 0; i < arrCopy.length / size; i++) {
+        let arr2 = arrCopy.splice(0, size);
+        while (arr2.length == size) {
+            newArray.push(arr2)
+            arr2 = arrCopy.splice(0, size);
+        }
+        let remainder = arr2.splice(0);
+        if (remainder != "") {
+            newArray.push(remainder)
+            return newArray;
+        } else { 
+            return newArray
+        }
+    }
+}
+pixelAreas = chunky(prepAreasArray, units);
+console.log(pixelAreas);
 
-heightStr = heightStr.padEnd((heightStr.length) * unitsTall, heightStr)
-alert(heightStr);
-
-
-container.style.cssText = `grid-template-columns: ${widthStr}; grid-template-rows: ${heightStr};`
-
+// Modifiying the CSS according to the variables for grid template columns, rows, and areas
+container.style.cssText = `grid-template-columns: ${pixelStr}; grid-template-rows: ${pixelStr};`
+container.style.cssText = `grid-template-areas: ${pixelAreas};`;
 console.log(container.style.cssText)
 
 // Idea 1: create an array that is unitsWide in length with the designated pixelwidth for columns, apply the same method for height and rows.
