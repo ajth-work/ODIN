5b98d5be916fb9a467e6cf4569f711749611da74
ajth-work <ajth.work@gmail.com>
2020-05-15 00:03:16 +0900

    finished reset button

--- projects/etch-a-sketch/etch.js
+++ projects/etch-a-sketch/etch.js
@@ -1,27 +1,22 @@
-function makeGrid() { 
+function makeGrid() {
+	const container = document.getElementById("container");
+	const count = prompt("How many?");
+	const cellPixelCount = ((500 - (2 * count)) / count);
+	console.log("cellPixelCount is roughly", cellPixelCount, "px.");
 
-const container = document.createElement("div");
-container.setAttribute("id", "container");
-body.appendChild(container);
+	for (let i = 1; i <= count * count; i++) {
+		let cell = document.createElement("div");
+		cell.setAttribute("id", "cell-" + i);
+		cell.setAttribute("class", "cell")
 
-const count = prompt("How many?");
-const cellPixelCount = ((500-(2*count))/count);
-console.log("cellPixelCount is roughly", Math.floor(cellPixelCount), "px.");
-
-
-for(let i = 1; i <= count*count; i++){
-	let cell = document.createElement("div");
-	cell.setAttribute("id", "cell-" + i);
-	cell.setAttribute("class", "cell")
-
-	cell.setAttribute("style", 
-	`
-	height: ${cellPixelCount}px; 
-	width: ${cellPixelCount}px; 
-	`
-	);
-	container.appendChild(cell);
-};
+		cell.setAttribute("style",
+			`
+			height: ${cellPixelCount}px; 
+			width: ${cellPixelCount}px; 
+			`
+		);
+		container.appendChild(cell);
+	};
 };
 
 makeGrid();
@@ -30,9 +25,17 @@ function colorize() {
 	const colorCells = document.querySelectorAll(".cell");
 	colorCells.forEach((colorCell) => {
 		colorCell.addEventListener("mouseover", (e) => {
-		e.target.style.background = "crimson";
-	})
+			e.target.style.background = "crimson";
+		})
 	})
 };
 
 colorize();
+
+const button = document.querySelector(".button");
+	button.addEventListener("click", function (e) {
+		const cells = document.querySelectorAll(".cell");
+		cells.forEach((aCell) => {
+			aCell.style.background = "lightgray";
+		})
+	});
