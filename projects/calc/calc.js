c5a469a275960b87eae34d02f76ec0b432a8e52f
ajth-work <ajth.work@gmail.com>
2020-05-17 16:46:45 +0900

    added sound

--- projects/calc/calc.js
+++ projects/calc/calc.js
@@ -6,6 +6,12 @@
 // divide
 // operate
 // display
+
+function play() {
+    const audio = document.getElementById("tink");
+    audio.play();
+}
+
 function displayText() {
     const display = document.getElementById("display");
     const buttons = document.querySelectorAll("button");
@@ -14,21 +20,25 @@ function displayText() {
         button.addEventListener("click", (e) => {
             switch (button.id) {
                 case "buttonDot":
+                    play();
                     console.log(button.id)
                     if (newStr.indexOf(".") == -1) {
                         newStr = newStr.concat(`${button.textContent}`);
                     }
                     break;
                 case "buttonClear":
+                    play();
                     console.log(button.id)
                     newStr = newStr.slice(0, -1);
                     console.log(newStr);
                     break;
                 case "buttonAllClear":
+                    play();
                     newStr = "";
                     console.log(newStr)
                     break;
                 default:
+                    play();
                     console.log(button.id)
                     if (newStr.length >= 9) {
                         display.style.wordWrap = "break-word";
